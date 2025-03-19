5cb6ebd01626c461343a92e9905a0ae1ed0bfc9e
Mike Lyons <lyons.mr@gmail.com>
2025-03-18 18:02:03 -0700

    update study habits and currency display

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   const [knowledge, setKnowledge] = useState(0)
   const [studyHabits, setStudyHabits] = useState(0)
   const [studyPowerUpgrades, setStudyPowerUpgrades] = useState(0)
+  var studyHabitsAvailable = true;
   var studyPowerUpgradeAvailable = true;
 
   return (
@@ -14,6 +15,11 @@ function App() {
       {/* logo glass moves everything down but adds drop shadow */}
       {/* <h1 className="logo">Welcome to the real world.</h1> */}
       <h1>Welcome to the real world.</h1>
+      <div className="card">
+        <p>Knowledge: {knowledge}</p>
+        <p>Study Habits: {studyHabits}</p>
+        {/* <p>Study Power Upgrades: {studyPowerUpgrades}</p> */}
+      </div>
       <div className="card">
         <button onClick={() => setKnowledge((knowledge) => knowledge + 1 + studyPowerUpgrades)}>
           {/* Knowledge is {knowledge} */}
@@ -25,14 +31,20 @@ function App() {
             <br />
             <br />
             {/* DO THIS WITH CSS */}
-            <button onClick={() => setStudyPowerUpgrades((studyPowerUpgrades) => studyPowerUpgrades + 1)}>
-              Study Power Upgrade
+            {/* <button onClick={() => setStudyPowerUpgrades((studyPowerUpgrades) => studyPowerUpgrades + 1)}>
+              Study Habits
+            </button> */}
+            <button onClick={() => setStudyHabits((studyHabits) => studyHabits + 1)}>
+              Study Habits
             </button>
           </>
         }
         <p>
           You know {knowledge === 0 ? "nothing" : knowledge + " things" }.
         </p>
+        {studyHabitsAvailable && <p>
+          You have {studyHabits === 0 ? "no" : studyHabits } study habit{studyHabits > 1 ? "s" : ""}.
+        </p>}
       </div>
     </>
   )
